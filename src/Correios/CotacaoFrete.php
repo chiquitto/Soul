ca96f838d5cb0f78147102b118f946d308b5cc96
Alisson Chiquitto <chiquitto@gmail.com>
2016-07-05 08:41:09 -0300

    CotacaoFreteTest

--- src/Correios/CotacaoFrete.php
+++ src/Correios/CotacaoFrete.php
@@ -28,142 +28,33 @@ class CotacaoFrete
     const SEDEX_SEM_CONTRATO = 40010;
     const URL_WS = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL';
     const MAX_PESO = 30;
+
     const MAX_VOLUME = 280000;
+    // const MAX_VOLUME = 1157625; // 105cm ^ 3 (max de largura, comprimento e altura)
+
     const MIN_ALTURA = 2;
     const MIN_LARGURA = 11;
     const MIN_COMPRIMENTO = 16;
-    const FORA_INTERVALO_DESATIVADO = '1';
+    const FORA_INTERVALO_DESATIVADO = 1;
 
     /**
      * Com base no site dos correios calcula em um pacote
      *  com medidas e peso maximo fornecidos pelos correios
      */
-    const FORA_DO_INTERVALO_MAX_UNICO = '2';
+    const FORA_INTERVALO_MAX_UNICO = 2;
 
     /**
      * Com base no site dos correios calcula quantidade de
      *  pacotes necessarias e cada pacote com medidas
      * e peso maximos fornecidos pelos correios
      */
-    const FORA_DO_INTERVALO_MAX_PACOTES = '3';
+    const FORA_INTERVALO_MAX_PACOTES = 3;
 
     /**
      * Com base no site dos correios calcula quantidade de pacotes necessarios
      * com medida e peso com base na media dos volumes
      */
-    const FORA_DO_INTERVALO_MEDIA = '4';
-
-    /**
-     * Codigo administrativo junto a ECT
-     *
-     * @var string
-     */
-    private $nCdEmpresa = '';
-
-    /**
-     * Senha para acesso ao servico
-     *
-     * @var string
-     */
-    private $sDsSenha = '';
-
-    /**
-     * Codigo do servico
-     *
-     * @var array
-     */
-    private $nCdServico = array();
-
-    /**
-     * CEP de origem sem hifen
-     *
-     * @var string
-     */
-    private $sCepOrigem;
-
-    /**
-     * CEP de destino sem hifen
-     *
-     * @var string
-     */
-    private $sCepDestino;
-
-    /**
-     * Peso da encomenda em kg, incluindo sua embalagem
-     *
-     * @var real
-     */
-    private $nVlPeso = 0;
-
-    /**
-     * Formato da encomenda.
-     * 1=Caixa/Pacote;
-     * 2=Prisma/Rolo;
-     *
-     * @var int
-     */
-    private $nCdFormato = 1;
-
-    /**
-     * Comprimento da encomenda em centimetros
-     *
-     * @var int
-     */
-    private $nVlComprimento = self::MIN_COMPRIMENTO;
-
-    /**
-     * Altura da encomenda em centimetros
-     *
-     * @var int
-     */
-    private $nVlAltura = self::MIN_COMPRIMENTO;
-
-    /**
-     * Largura da encomenda em centimetros
-     *
-     * @var int
-     */
-    private $nVlLargura = self::MIN_COMPRIMENTO;
-
-    /**
-     * Diametro da encomenda em centimetros
-     *
-     * @var int
-     */
-    private $nVlDiametro = 0;
-    private $nVlAcaoForaIntervalo = 0;
-
-    /**
-     * Indica se a encomenda sera entregue com o servico adicional mao propria
-     *
-     * @var boolean
-     */
-    private $sCdMaoPropria = 'N';
-
-    /**
-     * Indica se a encomenda sera entregue com o servico adicional valor declarado.
-     * Neste campo deve ser apresentado o valor declarado desejado, em R$.
-     *
-     * @var real
-     */
-    private $nVlValorDeclarado = 0;
-
-    /**
-     * Volume em cm3
-     *
-     * @var int
-     */
-    private $nVolume = 0;
-    private $qtPacotes = 1;
-
-    /**
-     * Indica se a encomenda sera entregue com o serviço adicional aviso de recebimento.
-     * S = Sim;
-     * N = Nao
-     *
-     * @var type
-     */
-    private $sCdAvisoRecebimento = 'N';
+    const FORA_INTERVALO_MEDIA = 4;
     public static $servicos = array(
         self::E_SEDEX_COM_CONTRATO => 'e-SEDEX, com contrato',
         self::E_SEDEX_EXPRESS_COM_CONTRATO => 'e-SEDEX Express, com contrato',
@@ -190,6 +81,103 @@ class CotacaoFrete
         self::SEDEX_HOJE_SEM_CONTRATO => 'SEDEX Hoje, sem contrato',
         self::SEDEX_SEM_CONTRATO => 'Entrega expressa',
     );
+    /**
+     * Codigo administrativo junto a ECT
+     *
+     * @var string
+     */
+    private $nCdEmpresa = '';
+    /**
+     * Senha para acesso ao servico
+     *
+     * @var string
+     */
+    private $sDsSenha = '';
+    /**
+     * Codigo do servico
+     *
+     * @var array
+     */
+    private $nCdServico = array();
+    /**
+     * CEP de origem sem hifen
+     *
+     * @var string
+     */
+    private $sCepOrigem;
+    /**
+     * CEP de destino sem hifen
+     *
+     * @var string
+     */
+    private $sCepDestino;
+    /**
+     * Peso da encomenda em kg, incluindo sua embalagem
+     *
+     * @var real
+     */
+    private $nVlPeso = 0;
+    /**
+     * Formato da encomenda.
+     * 1=Caixa/Pacote;
+     * 2=Prisma/Rolo;
+     *
+     * @var int
+     */
+    private $nCdFormato = 1;
+    /**
+     * Comprimento da encomenda em centimetros
+     *
+     * @var int
+     */
+    private $nVlComprimento = self::MIN_COMPRIMENTO;
+    /**
+     * Altura da encomenda em centimetros
+     *
+     * @var int
+     */
+    private $nVlAltura = self::MIN_COMPRIMENTO;
+    /**
+     * Largura da encomenda em centimetros
+     *
+     * @var int
+     */
+    private $nVlLargura = self::MIN_COMPRIMENTO;
+    /**
+     * Diametro da encomenda em centimetros
+     *
+     * @var int
+     */
+    private $nVlDiametro = 0;
+    private $nVlAcaoForaIntervalo = 0;
+    /**
+     * Indica se a encomenda sera entregue com o servico adicional mao propria
+     *
+     * @var boolean
+     */
+    private $sCdMaoPropria = 'N';
+    /**
+     * Indica se a encomenda sera entregue com o servico adicional valor declarado.
+     * Neste campo deve ser apresentado o valor declarado desejado, em R$.
+     *
+     * @var real
+     */
+    private $nVlValorDeclarado = 0;
+    /**
+     * Volume em cm3
+     *
+     * @var int
+     */
+    private $nVolume = 0;
+    private $qtPacotes = 1;
+    /**
+     * Indica se a encomenda sera entregue com o serviço adicional aviso de recebimento.
+     * S = Sim;
+     * N = Nao
+     *
+     * @var type
+     */
+    private $sCdAvisoRecebimento = 'N';
 
     /**
      * Adiciona um servico ao calculo do frete
@@ -204,20 +192,15 @@ class CotacaoFrete
     /**
      * Faz o calculo do frete
      *
-     * @param string $returnFormat O tipo do retorno.
-     * Pode ser JSON ou Objeto.
-     * O padrao eh Objeto.
-     *
-     * @return array|json
+     * @return array
      */
-    public function calc($returnFormat = 'object')
+    public function calc()
     {
         $parms = new \stdClass();
         $cm = '';
 
-        if (($this->nVlPeso > self::MAX_PESO) or ( $this->getVolume() > self::MAX_VOLUME)) {
+        if (($this->nVlPeso > self::MAX_PESO) or ($this->getVolume() > self::MAX_VOLUME)) {
             $this->foraDoIntervalo();
-            //$cm = ceil(pow($config['volumeItens'], 1 / 3));
         }
 
         $parms->nCdServico = join(',', $this->nCdServico);
@@ -258,19 +241,55 @@ class CotacaoFrete
         $objeto = $resposta->CalcPrecoPrazoResult->Servicos->cServico;
 
         foreach ($objeto as & $item) {
-            $item->Valor = ((int) strtr($item->Valor, array(',' => ''))) * ceil($this->qtPacotes);
-            $item->ValorSemAdicionais = ((int) strtr($item->ValorSemAdicionais, array(',' => ''))) * ceil($this->qtPacotes);
-            $item->ValorMaoPropria = ((int) strtr($item->ValorMaoPropria, array(',' => ''))) * ceil($this->qtPacotes);
-            $item->ValorAvisoRecebimento = ((int) strtr($item->ValorAvisoRecebimento, array(',' => ''))) * ceil($this->qtPacotes);
-            $item->ValorValorDeclarado = ((int) strtr($item->ValorValorDeclarado, array(',' => ''))) * ceil($this->qtPacotes);
+            $item->Valor = ((int)strtr($item->Valor, array(',' => ''))) * ceil($this->qtPacotes);
+            $item->ValorSemAdicionais = ((int)strtr($item->ValorSemAdicionais, array(',' => ''))) * ceil($this->qtPacotes);
+            $item->ValorMaoPropria = ((int)strtr($item->ValorMaoPropria, array(',' => ''))) * ceil($this->qtPacotes);
+            $item->ValorAvisoRecebimento = ((int)strtr($item->ValorAvisoRecebimento, array(',' => ''))) * ceil($this->qtPacotes);
+            $item->ValorValorDeclarado = ((int)strtr($item->ValorValorDeclarado, array(',' => ''))) * ceil($this->qtPacotes);
         }
 
-        switch ($returnFormat) {
-            case 'json':
-                return json_encode($objeto);
+        return $objeto;
+    }
+
+    public function calculaMaximoPacotes()
+    {
+        $pVolume = $this->getVolume() / self::MAX_VOLUME;
+        $pPeso = $this->nVlPeso / self::MAX_PESO;
+
+        $this->qtPacotes = max($pVolume, $pPeso);
+
+        $this->setVolume(self::MAX_VOLUME);
+        $this->setPesoKg(self::MAX_PESO);
+    }
+
+    public function calculaMedia()
+    {
+        $pVolume = $this->getVolume() / self::MAX_VOLUME;
+        $pPeso = $this->nVlPeso / self::MAX_PESO;
+
+        $this->qtPacotes = max($pVolume, $pPeso);
+        $this->nVolume = $this->nVolume / ceil($this->qtPacotes);
+        $this->nVlPeso = $this->nVlPeso / ceil($this->qtPacotes);
+    }
+
+    public function calculaValoresMaximos()
+    {
+        $this->qtPacotes = 1;
+        $this->setVolume(self::MAX_VOLUME);
+        $this->setPesoKg(self::MAX_PESO);
+    }
+
+    public function foraDoIntervalo()
+    {
+        switch ($this->nVlAcaoForaIntervalo) {
+            case self::FORA_INTERVALO_MAX_UNICO:
+                $this->calculaValoresMaximos();
                 break;
-            default:
-                return $objeto;
+            case self::FORA_INTERVALO_MAX_PACOTES:
+                $this->calculaMaximoPacotes();
+                break;
+            case self::FORA_INTERVALO_MEDIA:
+                $this->calculaMedia();
                 break;
         }
     }
@@ -290,37 +309,41 @@ class CotacaoFrete
         return $this->nVolume;
     }
 
+    /**
+     * Aceita valores de self::FORA_INTERVALO_*
+     * @param int $acao
+     */
     public function setAcaoForaIntervalo($acao)
     {
         $this->nVlAcaoForaIntervalo = $acao;
     }
 
     /**
-     * Informa a altura (Deve ser informado em Centimetros)
+     * Informa a altura (Deve ser informado em Centimetros).
+     * Valores entre 2 e 105 cm
      *
      * @param int
      */
     public function setAlturaCm($altura)
     {
-        $this->nVlAltura = (int) $altura;
+        $this->nVlAltura = (int)$altura;
 
         $this->nVolume = $this->nVlAltura * $this->nVlComprimento * $this->nVlLargura;
     }
 
     /**
-     * Informa se o calculo será feito com aviso de recebimento
+     * Define se o calculo será feito com aviso de recebimento
      *
-     * @param boolean
+     * @param bool $avisorecebimento
      */
     public function setAvisoRecebimento($avisorecebimento)
     {
-        if (is_bool($avisorecebimento)) {
-            $this->sCdAvisoRecebimento = $avisorecebimento;
-        } else {
-            $this->sCdAvisoRecebimento = (($avisorecebimento === 'S') or ( $avisorecebimento == '1'));
-        }
+        $this->sCdAvisoRecebimento = (bool)$avisorecebimento;
     }
 
+    /**
+     * @param string $cdEmpresa
+     */
     public function setCdEmpresa($cdEmpresa)
     {
         $this->nCdEmpresa = $cdEmpresa;
@@ -338,12 +361,13 @@ class CotacaoFrete
 
     /**
      * Informa o comprimento (Deve ser informado em Centimetros)
+     * Valores entre 16 e 105 cm
      *
      * @param int
      */
     public function setComprimentoCm($comprimento)
     {
-        $this->nVlComprimento = (int) $comprimento;
+        $this->nVlComprimento = (int)$comprimento;
 
         $this->nVolume = $this->nVlAltura * $this->nVlComprimento * $this->nVlLargura;
     }
@@ -355,7 +379,7 @@ class CotacaoFrete
      */
     public function setDiametroCm($diametro)
     {
-        $this->nVlDiametro = (int) $diametro;
+        $this->nVlDiametro = (int)$diametro;
     }
 
     public function setDsSenha($dsSenha)
@@ -365,12 +389,13 @@ class CotacaoFrete
 
     /**
      * Informa a largura (Deve ser informado em Centimetros)
+     * Valores entre 11 e 105 cm
      *
      * @param int
      */
     public function setLarguraCm($largura)
     {
-        $this->nVlLargura = (int) $largura;
+        $this->nVlLargura = (int)$largura;
 
         $this->nVolume = $this->nVlAltura * $this->nVlComprimento * $this->nVlLargura;
     }
@@ -382,16 +407,7 @@ class CotacaoFrete
      */
     public function setMaoPropria($maopropria)
     {
-        if (is_bool($maopropria)) {
-            $this->sCdMaoPropria = $maopropria;
-        } else {
-            $this->sCdMaoPropria = (($maopropria === 'S') or ( $maopropria == '1'));
-        }
-    }
-
-    public function setPesoKg($peso)
-    {
-        $this->nVlPeso = $peso;
+        $this->sCdMaoPropria = (bool)$maopropria;
     }
 
     /**
@@ -402,6 +418,11 @@ class CotacaoFrete
     public function setPesoG($peso)
     {
         $this->setPesoKg($peso / 1000);
+    }
+
+    public function setPesoKg($peso)
+    {
+        $this->nVlPeso = $peso;
     }
 
     /**
@@ -448,51 +469,5 @@ class CotacaoFrete
         $this->nVlAltura = $cm;
         $this->nVlLargura = $cm;
         $this->nVlComprimento = $cm;
-    }
-
-    public function calculaValoresMaximos()
-    {
-        $this->qtPacotes = 1;
-        $this->setVolume(self::MAX_VOLUME);
-        $this->setPesoKg(self::MAX_PESO);
-    }
-
-    public function calculaMaximoPacotes()
-    {
-        $pVolume = $this->getVolume() / self::MAX_VOLUME;
-        $pPeso = $this->nVlPeso / self::MAX_PESO;
-
-        $this->qtPacotes = max($pVolume, $pPeso);
-
-        $this->setVolume(self::MAX_VOLUME);
-        $this->setPesoKg(self::MAX_PESO);
-    }
-
-    public function calculaMedia()
-    {
-        $pVolume = $this->getVolume() / self::MAX_VOLUME;
-        $pPeso = $this->nVlPeso / self::MAX_PESO;
-
-        $this->qtPacotes = max($pVolume, $pPeso);
-        $this->nVolume = $this->nVolume / ceil($this->qtPacotes);
-        $this->nVlPeso = $this->nVlPeso / ceil($this->qtPacotes);
-    }
-
-    public function foraDoIntervalo()
-    {
-        switch ($this->nVlAcaoForaIntervalo) {
-            case self::FORA_INTERVALO_DESATIVADO;
-                return;
-                break;
-            case self::FORA_DO_INTERVALO_MAX_UNICO:
-                $this->calculaMaximo();
-                break;
-            case self::FORA_DO_INTERVALO_MAX_PACOTES:
-                $this->calculaMaximoPacotes();
-                break;
-            case self::FORA_DO_INTERVALO_MEDIA:
-                $this->calculaMedia();
-                break;
-        }
     }
 }
--- tests/Correios/CotacaoFreteTest.php
+++ tests/Correios/CotacaoFreteTest.php
@@ -9,16 +9,158 @@ class CotacaoFreteTest extends TestCase
 {
     public function testCalc()
     {
+        $prods = [
+            CotacaoFrete::PAC_SEM_CONTRATO,
+            CotacaoFrete::SEDEX_SEM_CONTRATO
+        ];
+
         $cotacao = new CotacaoFrete();
 
-        $cotacao->addServico(CotacaoFrete::PAC_SEM_CONTRATO);
-        $cotacao->addServico(CotacaoFrete::SEDEX_SEM_CONTRATO);
+        $cotacao->setServico([]);
+        foreach ($prods as $prod) {
+            $cotacao->addServico($prod);
+        }
 
         $cotacao->setCepDestino(58326000);
         $cotacao->setCepOrigem(87200426);
 
         $cotacao->setPesoKg(1);
+        $cotacao->setPesoG(1000);
+
+        $cotacao->setAlturaCm(20);
+        $cotacao->setLarguraCm(20);
+        $cotacao->setComprimentoCm(20);
+        $cotacao->setDiametroCm(10);
+
+        $cotacao->setAvisoRecebimento(true);
+        $cotacao->setMaoPropria(true);
+
+        $cotacao->setValorDeclaradoC(1000);
+        $cotacao->setValorDeclaradoR(10);
+
+        $cotacao->setCdEmpresa('');
+        $cotacao->setDsSenha('');
+
+        $cotacao->setAcaoForaIntervalo(CotacaoFrete::FORA_INTERVALO_DESATIVADO);
 
         $resultado = $cotacao->calc();
+
+        $this->assertCount(count($prods), $resultado);
+
+        foreach ($resultado as $prod) {
+            $this->assertObjectHasAttribute('Codigo', $prod);
+            $this->assertContains($prod->Codigo, $prods);
+        }
+    }
+
+    public function testCalcForaIntervaloMaxPacotes()
+    {
+        $prods = [
+            CotacaoFrete::PAC_SEM_CONTRATO,
+            CotacaoFrete::SEDEX_SEM_CONTRATO
+        ];
+
+        $cotacao = new CotacaoFrete();
+
+        foreach ($prods as $prod) {
+            $cotacao->addServico($prod);
+        }
+
+        $cotacao->setCepDestino(58326000);
+        $cotacao->setCepOrigem(87200426);
+
+        $cotacao->setPesoKg(80);
+
+        $cotacao->setAlturaCm(200);
+        $cotacao->setLarguraCm(200);
+        $cotacao->setComprimentoCm(200);
+
+        $cotacao->setAcaoForaIntervalo(CotacaoFrete::FORA_INTERVALO_MAX_PACOTES);
+
+        $resultado = $cotacao->calc();
+
+        $this->assertCount(count($prods), $resultado);
+
+        foreach ($resultado as $prod) {
+            $this->assertObjectHasAttribute('Codigo', $prod);
+            $this->assertContains($prod->Codigo, $prods);
+        }
+    }
+
+    public function testCalcForaIntervaloMaxUnico()
+    {
+        $prods = [
+            CotacaoFrete::PAC_SEM_CONTRATO,
+            CotacaoFrete::SEDEX_SEM_CONTRATO
+        ];
+
+        $cotacao = new CotacaoFrete();
+
+        foreach ($prods as $prod) {
+            $cotacao->addServico($prod);
+        }
+
+        $cotacao->setCepDestino(58326000);
+        $cotacao->setCepOrigem(87200426);
+
+        $cotacao->setPesoKg(80);
+
+        $cotacao->setAlturaCm(200);
+        $cotacao->setLarguraCm(200);
+        $cotacao->setComprimentoCm(200);
+
+        $cotacao->setAcaoForaIntervalo(CotacaoFrete::FORA_INTERVALO_MAX_UNICO);
+
+        $resultado = $cotacao->calc();
+
+        $this->assertCount(count($prods), $resultado);
+
+        foreach ($resultado as $prod) {
+            $this->assertObjectHasAttribute('Codigo', $prod);
+            $this->assertContains($prod->Codigo, $prods);
+        }
+    }
+
+
+
+    public function testCalcForaIntervaloMedia()
+    {
+        $prods = [
+            CotacaoFrete::PAC_SEM_CONTRATO,
+            CotacaoFrete::SEDEX_SEM_CONTRATO
+        ];
+
+        $cotacao = new CotacaoFrete();
+
+        foreach ($prods as $prod) {
+            $cotacao->addServico($prod);
+        }
+
+        $cotacao->setCepDestino(58326000);
+        $cotacao->setCepOrigem(87200426);
+
+        $cotacao->setPesoKg(80);
+
+        $cotacao->setAlturaCm(200);
+        $cotacao->setLarguraCm(200);
+        $cotacao->setComprimentoCm(200);
+
+        $cotacao->setAcaoForaIntervalo(CotacaoFrete::FORA_INTERVALO_MEDIA);
+
+        $resultado = $cotacao->calc();
+
+        $this->assertCount(count($prods), $resultado);
+
+        foreach ($resultado as $prod) {
+            $this->assertObjectHasAttribute('Codigo', $prod);
+            $this->assertContains($prod->Codigo, $prods);
+        }
+    }
+
+    public function testGetServicoNome()
+    {
+        $cotacao = new CotacaoFrete();
+
+        $this->assertEquals($cotacao->getServicoNome(CotacaoFrete::PAC_SEM_CONTRATO), CotacaoFrete::$servicos[CotacaoFrete::PAC_SEM_CONTRATO]);
     }
 }
